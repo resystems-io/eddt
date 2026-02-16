e6feb435b753de3e15d6d21f3d35eeeb6a441a18
Stewart Gebbie <stewart.gebbie@resystems.io>
2026-02-16 18:13:29 +0200

    deltaflow: write out Avro to OCF
    Create a simple OCF file from Avro records.

--- internal/deltaflow/avro_basics_test.go
+++ internal/deltaflow/avro_basics_test.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"log"
 
+	"os"
+
 	"github.com/hamba/avro/v2"
+	"github.com/hamba/avro/v2/ocf"
 )
 
 // __go:generate__ go install github.com/hamba/avro/v2/cmd/avrogen@latest
@@ -43,4 +46,68 @@ func Example_avro_basics() {
 	// Output:
 	// [54 6 102 111 111]
 	// {27 foo}
+}
+
+// Avro OCF
+//
+// In this example we generate a number of records and write them to an OCF file.
+//
+// We can then read this using duckdb:
+// ```
+// INSTALL avro;
+// LOAD avro;
+// SELECT * FROM read_avro('testdata.avro');
+// ```
+func Example_avro_ocf() {
+
+	// Create a new OCF file
+	f, err := os.Create("testdata.avro")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove("testdata.avro")
+
+	// Create a new encoder (using the string schema, rather than a pre-parsed schema)
+	enc, err := ocf.NewEncoder(string(avro_basics_schema), f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Write some records
+	for i := 0; i < 3; i++ {
+		in := SimpleRecord{A: int64(i), B: fmt.Sprintf("foo-%d", i)}
+		if err := enc.Encode(in); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if err := enc.Close(); err != nil {
+		log.Fatal(err)
+	}
+	f.Close()
+
+	// Read it back to verify
+	f, err = os.Open("testdata.avro")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	dec, err := ocf.NewDecoder(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for dec.HasNext() {
+		var out SimpleRecord
+		if err := dec.Decode(&out); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(out)
+	}
+
+	// Output:
+	// {0 foo-0}
+	// {1 foo-1}
+	// {2 foo-2}
 }
